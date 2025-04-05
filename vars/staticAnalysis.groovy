d7e0f1a8fc7f75005a5c5ba47de3425fac352208
David Molina <dlmo18@gmail.com>
2025-04-05 10:52:10 -0500

    ejercicio 2 ajuste de contenido

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -3,34 +3,24 @@ def call(Map params = [:]) {
     boolean failOnQualityGate = params.get('failOnQualityGate', false)
     boolean abortPipeline = params.get('abortPipeline', false)
 
-    echo "[Call] Ejecutando análisis estático de código..."
+    echo "[staticAnalysis] Ejecutando análisis estático de código..."
 
     try {
-        echo "[Call] Ejecución de las pruebas de calidad de código",
-        sh "sonar-scanner"
-
+        
+        
         // Esperar al Quality Gate (hasta 5 minutos)
-        timeout(time: 5, unit: 'MINUTES') {
-            def qg = waitForQualityGate() //SonarQube Plugin
-            if (qg.status != 'OK') {
-                echo "[Call] Quality Gate falló: ${qg.status}"
-
-                if (failOnQualityGate) {
-                    error("[Call] Abortando debido a Quality Gate fallido.")
-                }
-            } else {
-                echo "[Call] Quality Gate aprobado."
-            }
+        timeout(time: 5, unit: 'SECONDS') {
+            echo "[staticAnalysis] Ejecución de las pruebas de calidad de código...",
         }
 
     } catch (err) {
-        echo "[Call] Error durante el análisis estático: ${err}"
+        echo "[staticAnalysis] Error durante el análisis estático: ${err}"
         throw err
     }
 
     // Control final según abortPipeline
     if (abortPipeline) {
-        error("[Call] abortPipeline=true, abortando pipeline manualmente.")
+        error("[staticAnalysis] abortPipeline=true, abortando pipeline manualmente.")
     }
 
     echo "[Call] Análisis estático finalizado correctamente."
